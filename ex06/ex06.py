07592a27d832eeec0d96552ded113ebeae1d2f3e
CAD Client <CAD Client>
2016-04-01 16:08:56 +0900

    ex06 만우절 조크

--- ex06/ex06.py
+++ ex06/ex06.py
@@ -1,20 +1,23 @@
+# Creat x,y
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
 
-print x
-print y
+print (x)
+print (y)
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
+print ("I said: %r." % x)
+print ("I also said: '%s'." % y)
 
+# Manwoo jeol
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 
-print joke_evaluation % hilarious
+print (joke_evaluation % hilarious)
 
+# Nojam joke w+e
 w = "This is the left side of..."
 e = "a string with a right side."
 
-print w + e
+print (w + e)
