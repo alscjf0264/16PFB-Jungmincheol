61df794e9a3b8316121d2aeda6b2055c0aa15fb1
CAD Client <CAD Client>
2016-04-08 15:18:27 +0900

    , eraser check

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -3,7 +3,7 @@ from sys import argv
 print ("argv = " + str(argv))
 script, first, second, third = argv
 
-print ("The script is called %s", % script)
-print ("Your first variable is %s:", % first)
-print ("Your secnd varialbe is: %s", % second)
-print ("Your third variable is: %s", % third)
+print ("The script is called: %s" % script)
+print ("Your first variable is: %s" % first)
+print ("Your secnd varialbe is: %s" % second)
+print ("Your third variable is: %s" % third)
